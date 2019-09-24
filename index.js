65555be2f1fa2bd961b67c66f6bc1f5a5943d4c2
Alan Krajina <alankrajina88@gmail.com>
2019-09-24 15:12:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const user = 'alankrajina';
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return "ed047aff5b09f484bdad2d35b287d35b1085158b";
+  return "";
 }
 
 
@@ -25,12 +25,40 @@ function forkRepo() {
 
 function showResults(json) {
     $("#results").append(`<a href=${json.html_url}>${json.html_url}</a>`)
-
-
-function createIssue() {
-  //use this function to create an issue based on the values input in index.html
 }
 
-function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
-}
+    //function to post data
+    function createIssue() {
+      //use this function to create an issue based on the values input in index.html
+      const repo = `${user}/js-ajax-fetch-lab`;
+      const url = `${baseURL}/repos/${repo}/issues`;
+      const postData = {
+        title: document.getElementById('title').value,  // when <a> gets clicked this info gets saved to postData
+        body: document.getElementById('body').value     // when <a> gets clicked this info gets saved to postData
+      };
+    
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(postData),   // postData passed as a JSON string using JSON.stringify in the request body
+        headers: {
+          Authorization: `token ${getToken()}`
+        }
+      })
+        .then(res => res.json())
+        .then(json => getIssues());
+    }
+    
+    
+    //function to get data
+    function getIssues() {
+      //once an issue is submitted, fetch all open issues to see the issues you are creating
+      const repo = `${user}/js-ajax-fetch-lab`;
+     const url = `${baseURL}/repos/${repo}/issues`;
+     fetch(url, {
+       headers: {
+         Authorization: `token ${getToken()}`
+       }
+     })
+       .then(res => res.json())
+       .then(json => console.log(json));   // console log all issues
+    }
